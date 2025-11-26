f6d6e68f8f546e5eb5d7b823603d8c796da9c8b4
unknown <pedro29ortega@gmail.com>
2025-11-26 12:10:37 -0400

    mejoras

--- dart/lib/imc.dart
+++ dart/lib/imc.dart
@@ -1,6 +1,8 @@
 
 import 'dart:math';
 
+import 'package:colorize/colorize.dart';
+
 import '../util.dart' as util;
 
 // DEFINIR LAS NALGAS DEL PROGRAMA AQUI
@@ -20,4 +22,15 @@ void main(){
   imc = calcularIMC(peso, altura);
 
   print('Tu IMC es: ${imc.toStringAsFixed(2)}');
+
+  if(imc < 18.5){
+    print(Colorize('Estas bajo de peso').lightYellow());
+  } else if (imc >= 18.5 && imc < 24.9){
+    print(Colorize('Tienes un peso normal').lightGreen());
+  } else if (imc >= 25 && imc < 29.9){
+    print(Colorize('Tienes sobrepeso').lightRed());
+  } else {
+    print(Colorize('OBESIDAD, ALERTA GORDO').red());
+
+  }
 }
